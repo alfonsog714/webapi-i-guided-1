73338f0b207b4fde086ccdef364f6ac738e52f10
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-11 12:30:57 -0700

    Added more CRUD methods

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const express = require("express");
 
 const server = express();
+const port = 5000;
 
-const db = require("./data/hubs-model.js"); // enables us to use the functions made in hubs-model. Similar to services or repos in Java
+const Hubs = require("./data/hubs-model.js"); // enables us to use the functions made in hubs-model. Similar to services or repos in Java
+
+server.use(express.json()); // <<<< to parse JSON in POST
 
 server.get("/", (req, res) => {
   res.send("Hello world!");
@@ -11,7 +14,7 @@ server.get("/", (req, res) => {
 
 // The R in CRUD
 server.get("/hubs", (req, res) => {
-  db.find()
+  Hubs.find()
     .then(hubs => {
       res.status(200).json(hubs);
     })
@@ -20,7 +23,56 @@ server.get("/hubs", (req, res) => {
     });
 });
 
-const port = 5000;
+// The C in CRUD
+server.post("/hubs", (req, res) => {
+  // axios.post(url, data) < data shows up as req.body
+  const hubInfo = req.body;
+  console.log(hubInfo);
+
+  Hubs.add(hubInfo)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// The D in CRUD
+server.delete("/hubs/:id", (req, res) => {
+  const { id } = req.params;
+
+  Hubs.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "Can't find that hub." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// The U in CRUD
+server.put("/hubs/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  Hubs.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json(updated);
+      } else {
+        res.status(404).json({ message: "Can't find that hub." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`));
 
 // install express: npm i express
